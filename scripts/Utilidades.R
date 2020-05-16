7e73c7523d61f0e8f56e4d58bc70d0c8ff44247e
msantos <maria@frogames.es>
2020-05-16 12:10:25 +0200

    Script de utilidades

---
+++ scripts/Utilidades.R
@@ -0,0 +1,15 @@
+#Instalar libreria
+install.packages("tidyverse", dependencies = T)
+
+#Cargar libreria 
+library(tidyverse)
+
+#Versión de libreria cargadas y versión de R necesaria.
+pkgs <- as.data.frame(installed.packages(), stringsAsFactors = FALSE, row.names = FALSE)
+pkgs[, c("Package", "Version", "Built")]
+
+#Actualizar versión de R
+install.packages("installr")
+library(installr)
+updateR()
+
